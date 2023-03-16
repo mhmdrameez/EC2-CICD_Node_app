3c6343fd4586eb31b960923f0dc8705fecf66303
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2023-03-16 17:17:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
   // Sending This is the home page! in the page
   res
     .status(200)
-    .send(`This Page is running in AWS EC2 CI/CD WITH Github Actions is the home page and test runninsgs on sdfdsfds ${PORT}`);
+    .send(`This Page is running in AWS EC2 CI/CD WITH Github Actions is the home page and test runninsgs on SDE ${PORT}`);
 });
 
 // Listening to the port 
