10ed1cb6131cf307d7c91bab9c0525dc1e7697f1
rameez <rameez@newagesmb.com>
2023-03-16 14:19:37 +0530

    updates

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+// route
+let PORT = process.env.PORT ? process.env.PORT : 8000;
+app.get("/", (req, res) => {
+  // Sending This is the home page! in the page
+  res
+    .status(200)
+    .send(`This Page is running in AWS EC2 CI/CD WITH Github Actions is the home page and running on ${PORT}`);
+});
+
+// Listening to the port 
+
+console.log(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running...`);
+});
